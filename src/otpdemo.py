3792c1a0fe3f2f60dbba841e9e14916aa22d6207
Matthew Cole <mcole8@binghamton.edu>
2016-11-21 14:10:10 -0500

    Added QRCode reading framework

--- src/otpdemo.py
+++ src/otpdemo.py
@@ -9,6 +9,13 @@ import time
 import sys
 import otp
 import argparse
+try:
+	from PIL import Image
+	import zbarlight
+except(ImportError):
+	print("pyOTP requires zbarlight.", file=sys.stderr)
+	print("See: https://github.com/Polyconseil/zbarlight for more information", file=sys.stderr)
+	sys.exit(0)
 
 """
 Utility functions
@@ -50,14 +57,32 @@ if __name__ == "__main__":
 
 	# Argument parsing
 	parser = argparse.ArgumentParser(description='Demonstrate the TOTP algorithm')
-	parser.add_argument('qrfile', help="path to the shared secret key QR Code")
+	parser.add_argument('qrfile', help="path to the shared secret key QR Code .png file")
 	parser.add_argument('-d', '--digits', help="number of digits in output", type=int, choices = [6, 7, 8], default=6)
 	parser.add_argument('-t', '--time', help="time step in seconds between outputs", type=int, default=30)
-	parser.add_argument('-m' '--mode', help="cryptographic hash function selection", choices=["sha1", "sha256", "sha512"], default="sha1")
+	parser.add_argument('-m', '--mode', help="cryptographic hash function selection", choices=["sha1", "sha256", "sha512"], default="sha1")
 
 	args = parser.parse_args()
+	print(args)
 	digit = args.digits
 	x = args.time
+	mode = args.mode
+
+	### QR Code reading using zbarlite
+	try:
+		with open(args.qrfile, 'rb') as image_file:
+    		image = Image.open(image_file)
+    		image.load()
+		codes = zbarlight.scan_codes('qrcode', image)
+	except(IOError):
+		print("Cannot open %s" % args.qrfile, file=sys.stderr)
+		exit(0)
+
+	if codes:
+		token = int(codes)
+	else:
+		print("Did not find a valid QR Code in %s" % args.qrfile, file=sys.stderr)
+		exit(0)
 
 	# Print the header
 	print(_header())
@@ -66,7 +91,7 @@ if __name__ == "__main__":
 	now = int(time.time())
 	next = now - (now % x) + x
 	ts = otp.T(now, x)
-	totp = otp.TOTP(otp.TOKEN,ts,digit)
+	totp = otp.TOTP(token,ts,digit)
 
 	# Continue generating TOTP passwords until halted by user
 	while(True):
@@ -89,4 +114,4 @@ if __name__ == "__main__":
 		now = next
 		next += x
 		ts = otp.T(now, x)
-		totp = otp.TOTP(otp.TOKEN,ts,digit)
+		totp = otp.TOTP(token,ts,digit)
